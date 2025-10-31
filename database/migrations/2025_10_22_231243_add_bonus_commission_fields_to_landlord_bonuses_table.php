c995998646de64cf6ff10d4f992ae98ea6f940d5
ali <ali.abouel3aish@gmail.com>
2025-10-31 14:34:52 +0000

    map

--- database/migrations/2025_10_22_231243_add_bonus_commission_fields_to_landlord_bonuses_table.php
+++ database/migrations/2025_10_22_231243_add_bonus_commission_fields_to_landlord_bonuses_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('landlord_bonuses', function (Blueprint $table) {
-            //
-        });
+        
+        return;
     }
 
     /**
