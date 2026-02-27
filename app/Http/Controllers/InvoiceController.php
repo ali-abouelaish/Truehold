40a3abe6fc978c2cac59747516b2b7d3778e7838
ali <ali.abouel3aish@gmail.com>
2026-02-27 19:03:01 +0200

    horizon invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -53,8 +53,8 @@ class InvoiceController extends Controller
             // Set company details (you can make these configurable)
             $invoice->company_name = 'Truehold Group Limited';
             $invoice->company_address = 'Business Banking';
-            $invoice->account_holder_name = 'TRUEHOLD GROUP LTD';
-            $invoice->account_number = '63935841';
+            $invoice->account_holder_name = 'HORIZON DREAMS LIMITED';
+            $invoice->account_number = '03630064';
             $invoice->sort_code = '20-41-50';
             $invoice->bank_name = 'Business Banking';
             
